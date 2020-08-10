27cf973e37aaf961bd26ed6929940d82701d9956
div-antk <51283468+owaii@users.noreply.github.com>
2020-08-10 17:45:53 +0900

    画像のサムネイル表示

--- public/js/app.js
+++ public/js/app.js
@@ -37266,6 +37266,8 @@ module.exports = function(module) {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+__webpack_require__(/*! ./users */ "./resources/js/users.js");
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
@@ -37310,6 +37312,32 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+/***/ }),
+
+/***/ "./resources/js/users.js":
+/*!*******************************!*\
+  !*** ./resources/js/users.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+$(document).on("change", "#file_photo", function (e) {
+  var reader;
+
+  if (e.target.files.length) {
+    reader = new FileReader();
+
+    reader.onload = function (e) {
+      var userThumbnail;
+      userThumbnail = document.getElementById('thumbnail');
+      $("#userImgPreview").addClass("is-active");
+      userThumbnail.setAttribute('src', e.target.result);
+    };
+
+    return reader.readAsDataURL(e.target.files[0]);
+  }
+});
 
 /***/ }),
 
