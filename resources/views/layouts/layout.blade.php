105bced52c2af253e80b6e24d8e7c854a773f07d
div-antk <51283468+owaii@users.noreply.github.com>
2020-08-10 16:57:04 +0900

    トップページのCSS適用

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -20,7 +20,10 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+
+    @yield('content')
+
+    {{-- <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -75,6 +78,6 @@
         <main class="py-4">
             @yield('content')
         </main>
-    </div>
+    </div> --}}
 </body>
 </html>
