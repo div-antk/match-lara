76ebab700e7c46b6d58e26151eb8f6cea4a11d42
div-antk <51283468+owaii@users.noreply.github.com>
2020-08-23 17:19:51 +0900

    リレーションを組む

--- app/ChatRoom.php
+++ app/ChatRoom.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChatRoom extends Model
 {
-    //
+    public function chatRoomUsers()
+    {
+        return $this->hasMany('App\ChatRoomUser');
+    }
+
+    public function chatMessages()
+    {
+        return $this->hasMany('App\ChatMessage');
+    }
 }
--- app/ChatRoomUser.php
+++ app/ChatRoomUser.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChatRoomUser extends Model
 {
-    //
+    protected $fillable = ['chat_room_id', 'user_id'];
+
+    public function chatRoom()
+    {
+        return $this->belongsTo('App\ChatRoom');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
